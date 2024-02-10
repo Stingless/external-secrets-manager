06f4ba6574b84b15f22ce600777d25c985596b8d
rahulk789 <rahul.u.india@gmail.com>
2024-02-10 21:25:34 +0530

    added logging

--- main.go
+++ main.go
@@ -16,20 +16,29 @@ func EsGenerator() (string) {
 
 	client, err := vault.NewClient(config)
 	if err != nil {
-		log.Fatalf("unable to initialize Vault client: %v", err)
+		log.Fatalf("Unable to initialize Vault client: %v", err)
 	}
 
-	client.SetToken("")
+	client.SetToken("hvs.QjdJ8dXOWhbB1poIjxu40jfu")
     
     var f = ""
     var remoteref = ""
     namespace, err := client.KVv2("es-manager").Get(context.Background(), "namespace")
 	if err != nil {
-		log.Fatalf("unable to read secret: %v", err)
+		log.Fatalf("Unable to read secret from es-manager/namespace: %v", err)
 	}
 
 	for vaultpath, k8namespace := range namespace.Data {
-        _ = os.Mkdir("vault-es/"+fmt.Sprintf("%v",vaultpath), os.ModePerm)
+        
+        if _, err := os.Stat("vault-es/"+fmt.Sprintf("%v",vaultpath)); err == nil {
+            fmt.Println("Directory already exists: vault-es/"+vaultpath)
+        } else if os.IsNotExist(err) {
+            _ = os.Mkdir("vault-es/"+fmt.Sprintf("%v",vaultpath), os.ModePerm)
+            fmt.Println("Created new directory: vault-es/"+vaultpath)
+        } else {
+            fmt.Println("Amogus: vault-es/"+vaultpath)
+ 
+        }
         
         vaultpathlist := strings.Split(vaultpath, "/")
         vaultkv := vaultpathlist[0]
@@ -37,7 +46,7 @@ func EsGenerator() (string) {
         vaultpathnested := strings.Join([]string(vaultpathtemp), "/")
         secretpaths, err := client.Logical().List(vaultkv+"/metadata/"+vaultpathnested)
         if err != nil {
-	        log.Fatalf("unable to read secret: %v", err)
+	        log.Fatalf("Unable to read secret file path: %v", err)
 	    }
 
         for _, value := range secretpaths.Data {
@@ -53,6 +62,7 @@ metadata:
 
             secretkeymap, err := client.KVv2(vaultpath).Get(context.Background(),fmt.Sprintf("%v",fname))
 	        if err != nil {
+                fmt.Println("Skipping: vault-es/"+vaultpath+"/"+fmt.Sprintf("%v",fname))
 		        continue
 	        }
 
@@ -75,18 +85,26 @@ f = f + remoteref +`
     deletionPolicy: Retain
     name: `+fmt.Sprintf("%v",fname)+"-secret"
     
-    esfile, err := os.Create("vault-es/"+fmt.Sprintf("%v",vaultpath)+"/"+fmt.Sprintf("%v",fname) + "-es.yaml")
-    if err != nil {
-        panic(err)
-    }
-    defer func() {
-        if err := esfile.Close(); err != nil {
+    if _, err := os.Stat("vault-es/"+fmt.Sprintf("%v",vaultpath)+"/"+fmt.Sprintf("%v",fname)+"-es.yaml"); err == nil {
+        fmt.Println("Configured external secret yaml file in path: vault-es/"+vaultpath+"/"+fmt.Sprintf("%v",fname)+"-es.yaml")
+    } else if os.IsNotExist(err) {
+        esfile, err := os.Create("vault-es/"+fmt.Sprintf("%v",vaultpath)+"/"+fmt.Sprintf("%v",fname) + "-es.yaml")
+        if err != nil {
             panic(err)
         }
-    }()
-    if _, err := esfile.WriteString(f); err != nil {
-        panic(err)
+        defer func() {
+            if err := esfile.Close(); err != nil {
+                panic(err)
+            }
+        }()
+        if _, err := esfile.WriteString(f); err != nil {
+            panic(err)
+        }
+        fmt.Println("Created external secret yaml file in path: vault-es/"+vaultpath+"/"+fmt.Sprintf("%v",fname)+"-es.yaml")
+    } else {
+        fmt.Println("Amogus path: vault-es/"+vaultpath+"/"+fmt.Sprintf("%v",fname))
     }
+  
     remoteref = ""
     f = ""
             }
